59c95bf5438c766e80225552ca5211bebf970f18
T <marcelo.alvarez.e@usach.cl>
2021-11-25 21:30:41 -0300

    Some basic offsets

---
+++ Aogami.SMTV/SaveData/SMTVGameSaveDataOffsets.cs
@@ -0,0 +1,19 @@
+﻿namespace Aogami.SMTV.SaveData
+{
+    public static class SMTVGameSaveDataOffsets
+    {
+        // Protagonist Name
+        public const int FirstName_a = 1036;
+        public const int FirstName_b = 2308;
+        public const int FirstName_c = 2352;
+        public const int LastName = 2332;
+        public const int FullName_a = 2372;
+
+        // General
+        public const int GameDifficulty = 1072;
+        public const int Macca = 12144;
+
+        // Items
+        public const int ItemOffset = 13216; // byte (0 to 255)
+    }
+}
