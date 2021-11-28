aa81340e2903f70518107f98f1cbd2759f6aab31
T <marcelo.alvarez.e@usach.cl>
2021-11-28 10:26:50 -0300

    Add sprites

---
+++ Aogami.SMTV/GameData/SMTVSpriteUtil.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace Aogami.SMTV.GameData
+{
+    public static class SMTVSpriteUtil
+    {
+        public static Bitmap? GetSprite(string SpriteName)
+        {
+            return Resources.ResourceManager.GetObject(SpriteName) as Bitmap;
+        }
+    }
+}
