2f00e65af965749bc2ff2d44d749229cd5136851
T <marcelo.alvarez.e@usach.cl>
2021-12-05 21:50:10 -0300

    Alignment data

---
+++ Aogami.SMTV/GameData/SMTVAlignment.cs
@@ -0,0 +1,22 @@
+﻿namespace Aogami.SMTV.GameData
+{
+    public class SMTVAlignment
+    {
+        private readonly SMTVAlignmentAttribute1 _attribute1;
+        private readonly SMTVAlignmentAttribute2 _attribute2;
+
+        public byte Attribute1 => (byte)_attribute1;
+        public byte Attribute2 => (byte)_attribute2;
+
+        public SMTVAlignment(byte attribute1, byte attribute2)
+        {
+            _attribute1 = (SMTVAlignmentAttribute1)attribute1;
+            _attribute2 = (SMTVAlignmentAttribute2)attribute2;
+        }
+        
+        public override string ToString()
+        {
+            return $"{_attribute1}-{_attribute2.ToString().ToLower()}";
+        }
+    }
+}
---
+++ Aogami.SMTV/GameData/SMTVAlignmentAttributes.cs
@@ -0,0 +1,16 @@
+﻿namespace Aogami.SMTV.GameData
+{
+    public enum SMTVAlignmentAttribute1
+    {
+        Light = 0,
+        Neutral = 1,
+        Dark = 2
+    }
+
+    public enum SMTVAlignmentAttribute2
+    {
+        Law = 0,
+        Neutral = 1,
+        Chaos = 2
+    }
+}
